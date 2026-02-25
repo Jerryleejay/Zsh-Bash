230d67d87b68c3472bf3a2f587a0df73dffa9e05
KC3KMV <65052320+TerryClaiborne@users.noreply.github.com>
2026-02-25 11:40:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright (C) 2026 Terry L. Claiborne, KC3KMV
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # Check if script is run as root
 if [ "$EUID" -ne 0 ]; then 
   echo "Please run with sudo: sudo bash install.sh"
