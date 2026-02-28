bcde63aaa22348716ba2298d5a803325756dd71e
KC3KMV <65052320+TerryClaiborne@users.noreply.github.com>
2026-02-28 15:54:47 -0500

    Update install.sh with z-on and z-off scripts using markers.

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Updated z-off script with safer sed pattern
+# z-on script to add zsh launcher block to .bashrc
+# BEGIN Z-ON LAUNCHER
+if [ -z "$ZSH_LAUNCHER_LOADED" ]; then
+    export ZSH_LAUNCHER_LOADED=true
+    # Add your zsh launcher lines below this line
+    echo 'source ~/path/to/your/zsh_launcher.sh' >> ~/.bashrc
+fi
+# END Z-ON LAUNCHER
 
-# Start marker
-START_MARKER='# BEGIN z-off'
-END_MARKER='# END z-off'
 
-# Use sed to modify the .bashrc without orphaning fi statements
-sed -i "/$START_MARKER/,/$END_MARKER/d" ~/.bashrc
+# z-off script to remove zsh launcher block from .bashrc
+# Using `sed` to remove from BEGIN to END markers inclusively
+if [ -n "$ZSH_LAUNCHER_LOADED" ]; then
+    sed -i '/# BEGIN Z-ON LAUNCHER/,/# END Z-ON LAUNCHER/d' ~/.bashrc
+    unset ZSH_LAUNCHER_LOADED
+fi
